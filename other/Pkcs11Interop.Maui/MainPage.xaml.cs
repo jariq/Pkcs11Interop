5e11e327b4a7424df7b56499496386dab043a102
Jaroslav Imrich <jimrich@jimrich.sk>
2025-01-23 19:46:21 +0100

    Setup MAUI to use Pkcs11Interop

--- other/Pkcs11Interop.Maui/MainPage.xaml.cs
+++ other/Pkcs11Interop.Maui/MainPage.xaml.cs
@@ -1,8 +1,11 @@
-﻿namespace Pkcs11Interop.Maui;
+﻿using Net.Pkcs11Interop.Common;
+using Net.Pkcs11Interop.HighLevelAPI;
+
+namespace Pkcs11Interop.Maui;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	// int count = 0;
 
 	public MainPage()
 	{
@@ -11,12 +14,28 @@ public partial class MainPage : ContentPage
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
+		/*
 		count++;
 
 		if (count == 1)
 			CounterBtn.Text = $"Clicked {count} time";
 		else
 			CounterBtn.Text = $"Clicked {count} times";
+		*/
+
+		try
+		{
+			var libraryPath = "./pkcs11-mock/macos/pkcs11-mock.dylib";
+			var factories = new Pkcs11InteropFactories();
+			var library = factories.Pkcs11LibraryFactory.LoadPkcs11Library(factories, libraryPath, AppType.MultiThreaded);
+			var info = library.GetInfo();
+
+			CounterBtn.Text = info.ManufacturerId;
+		}
+		catch (Exception ex)
+		{
+			CounterBtn.Text = ex.Message;
+		}
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
